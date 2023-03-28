b2cb99bc5ead5ab14a403f7d69763d3f30fe5602
workPC <123@mail.ru>
2023-03-28 16:25:48 +0300

    reviews+

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from .models import Cakes, Review
 
 
 def index(request):
-    review = Review.objects.all()
-    return render(request, 'main/index.html', {'review': review})
+    review_1 = Review.objects.first()
+    review_2 = Review.objects.all()[1]
+    return render(request, 'main/index.html', {'review_1': review_1, 'review_2': review_2})
 
 def about(request):
     return render(request, 'main/about-us.html')
