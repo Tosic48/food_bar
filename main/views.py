b094185fffcc479fed8589d15d704d9641273869
“Anton_Mint” <antonsuhodolski@tut.by>
2023-04-03 00:29:38 +0300

    finished review

--- main/views.py
+++ main/views.py
@@ -3,9 +3,8 @@ from .models import Cakes, Review
 
 
 def index(request):
-    review_1 = Review.objects.first()
-    review_2 = Review.objects.all()[1]
-    return render(request, 'main/index.html', {'review_1': review_1, 'review_2': review_2})
+    review = Review.objects.all()
+    return render(request, 'main/index.html', {'review': review})
 
 def about(request):
     return render(request, 'main/about-us.html')
