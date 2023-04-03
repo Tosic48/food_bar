5a79fe0429f0201e5d84c76d3e501132413353c8
workPC <123@mail.ru>
2023-04-03 11:36:28 +0300

    metamorf class

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import index, about, contact, menu
+from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about-us', about, name='about-us'),
-    path('contacts', contact, name='contact'),
-    path('menu', menu, name='menu'),
+    path('about-us', AboutView.as_view(), name='about-us'),
+    path('contacts', ContactView.as_view(), name='contact'),
+    # path('menu', menu, name='menu'),
+    path('menu', MenuView.as_view(), name='menu'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+
 from .models import Cakes, Review
 
 
@@ -6,14 +8,20 @@ def index(request):
     review = Review.objects.all()
     return render(request, 'main/index.html', {'review': review})
 
-def about(request):
-    return render(request, 'main/about-us.html')
+class AboutView(TemplateView):
+    template_name = 'main/about-us.html'
 
-def contact(request):
-    return render(request, 'main/contact.html')
+class ContactView(TemplateView):
+    template_name = 'main/contact.html'
 
-def menu(request):
-    cakes = Cakes.objects.all()
-    return render(request, 'main/menu.html', {'cakes': cakes})
+# def menu(request):
+#     cakes = Cakes.objects.all()
+#     return render(request, 'main/menu.html', {'cakes': cakes})
 
+class MenuView(TemplateView):
+    template_name = 'main/menu.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cakes'] = Cakes.objects.all()
+        return context
