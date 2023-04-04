8e9e83bc2a1a9520329a8e0363eae355ea74f2aa
workPC <123@mail.ru>
2023-04-04 16:20:15 +0300

    set adminka +

--- main/admin.py
+++ main/admin.py
@@ -13,6 +13,12 @@ class PersonAdmin(admin.ModelAdmin):
     list_editable = ('vegetarion_type', 'price',)
     list_filter = ('vegetarion_type',)
     search_fields = ('composition',)
+    actions = ['make_vegeterian']
+
+
+    @admin.action(description='Сделать вегетерианскими')
+    def make_vegeterian(modeladmin, request, queryset):
+        queryset.update(vegetarion_type='vegeterian')
 
 
     def preview_photo(self, obj):
@@ -21,6 +27,11 @@ class PersonAdmin(admin.ModelAdmin):
 
 @admin.register(Review)
 class PersonAdmin(admin.ModelAdmin):
+    fields = ('text', 'details', 'customers', 'photo', 'preview_photo')
     list_display = ('pk', 'customers', 'text', 'details')
+    readonly_fields = ('preview_photo',)
     list_display_links = ('customers',)
+
+    def preview_photo(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}" width="150" />')
 
