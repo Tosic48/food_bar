588c45a9e9e555a24eef86336962dd33b59110da
workPC <123@mail.ru>
2023-04-04 15:25:24 +0300

    set adminka +

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from main.models import Cakes, Review
 
 
 @admin.register(Cakes)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    fields = ('title', 'composition', 'price', 'rating', 'vegetarion_type', 'layer_number', 'photo', 'preview_photo')
+    readonly_fields = ('preview_photo',)
+    list_display = ('pk', 'title', 'composition', 'price', 'vegetarion_type')
+    list_display_links = ('title',)
+    list_editable = ('vegetarion_type',)
+    list_filter = ('vegetarion_type',)
+    search_fields = ('composition',)
+
+    def preview_photo(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}" width="150" />')
+
 
 @admin.register(Review)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pk', 'customers', 'text', 'details')
+    list_display_links = ('customers',)
+
