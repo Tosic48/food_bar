17c3d87ef5f8ef3ea010d570be5f2279ea126975
myHome <123@mail.ru>
2023-04-11 23:26:43 +0300

    created forms Contact me

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('contacts/', ContactView.as_view(), name='contact'),
     path('menu/', MenuListView.as_view(), name='menu'),
     path('review_created/', ReviewCreatedView.as_view(), name='review_created'),
-    # path('cake_detail/<int:pk>', cake_detail, name='cake_detail'),
     path('cake_detail/<int:pk>', CakeDetailView.as_view(), name='cake_detail'),
     path('accounts/login/', FBLoginView.as_view(), name='login'),
     path('accounts/logout/', FBLogoutView.as_view(), name='logout'),
